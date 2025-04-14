f3dc8710413418889a08cc8f33e41d099eaa45f9
Abhinav Kota <abhinav.kota06@gmail.com>
2025-04-13 21:48:21 -0400

    Set up react project and tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-green-500 text-white text-4xl p-10 font-bold">
+        Tailwind is working 🎉
+      </div>
+    </>
+  );
+}
+
+export default App;
