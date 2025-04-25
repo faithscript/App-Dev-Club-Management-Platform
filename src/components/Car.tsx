5bad5f2f4a98c0a396f0c093058100b2bc88d550
amytxc <tsaiax8@gmail.com>
2025-04-25 18:58:06 -0400

    Car component

---
+++ src/components/Car.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface CarProps {
+  name: string;
+  points: number;
+  carImage: string;
+}
+
+const Car: React.FC<CarProps> = ({ name, points, carImage }) => {
+  return (
+    <div className="flex flex-col items-center mx-4">
+      <img src={carImage} alt={`${name} car`} className="w-28 h-16 object-contain" />
+      <p className="text-sm mt-2 font-semibold">{name}</p>
+      <p className="text-xs text-gray-600">Points: {points}</p>
+    </div>
+  );
+};
+
+export default Car;
