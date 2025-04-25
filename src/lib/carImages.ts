4d561e92b71c84e4473f1958d6413aed1f7fcca6
amytxc <tsaiax8@gmail.com>
2025-04-25 18:30:35 -0400

    car images

---
+++ src/lib/carImages.ts
@@ -0,0 +1,20 @@
+import car1 from "../assets/1.png"; 
+import car2 from "../assets/2.png"; 
+import car3 from "../assets/3.png"; 
+import car4 from "../assets/4.png"; 
+import car5 from "../assets/5.png"; 
+import car6 from "../assets/6.png"; 
+import car7 from "../assets/7.png"; 
+import car8 from "../assets/8.png"; 
+import car9 from "../assets/9.png"; 
+import car10 from "../assets/10.png"; 
+import car11 from "../assets/11.png"; 
+import car12 from "../assets/12.png"; 
+import car13 from "../assets/13.png"; 
+import car14 from "../assets/14.png"; 
+import car15 from "../assets/15.png"; 
+
+export const carImages = [
+    car1, car2, car3, car4, car5, car6, car7, car8, 
+    car9, car10, car11, car12, car13, car14, car15
+];
