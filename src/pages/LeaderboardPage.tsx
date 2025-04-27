2c9a95828f606b8a6be969fd7d0b50ebdf500f4b
amytxc <tsaiax8@gmail.com>
2025-04-27 17:38:55 -0400

    leaderboard connected to database and changed car appearance

--- src/pages/LeaderboardPage.tsx
+++ src/pages/LeaderboardPage.tsx
@@ -20,7 +20,6 @@ interface MentorGroup {
 }
 
 // Get from API
-
 const LeaderboardPage: React.FC = () => {
   const [groups, setGroups] = useState<MentorGroup[]>([]);
   const [loading, setLoading] = useState(true);
@@ -54,51 +53,15 @@ const LeaderboardPage: React.FC = () => {
     }
   };
 
-// Version that uses mock data
-/*
-// Import mock users for testing
-import { mockUsers } from '../lib/mockUsers';
-
-const LeaderboardPage: React.FC = () => {
-  const [groups, setGroups] = useState<MentorGroup[]>([]);
-
-  useEffect(() => {
-    const groupMap: Record<string, number> = {};
-
-    // Using mock data to test
-    // Filter users for mentors
-    const mentors = mockUsers.filter(user => user.accountType === "Mentor");
-
-    mentors.forEach(mentor => {
-      const name = mentor.fullName;
-      const points = mentor.points;
-      groupMap[name] = points;
-    });
-    
-    // Sort mentor groups by points earned
-    const sortedGroups = Object.entries(groupMap)
-      .map(([name, points], idx) => ({
-        name,
-        points,
-        carImage: carImages[idx % carImages.length],
-      }))
-      .sort((a, b) => a.points - b.points);
-
-    setGroups(sortedGroups);
-  }, []);
-*/
-
   return (
     <div className="min-h-screen flex flex-col bg-white">
       <NavBar /> 
       {/* Header and top 3 mentor groups */}
-      <h1 className="text-3xl font-bold text-center mt-60 mb-4">Leaderboard</h1>
+      <h1 className="text-3xl font-bold text-center mt-40 mb-4">Leaderboard</h1>
       <div className="text-center mb-8">
-        {groups.length >=3 && (
+        {groups.length >=1 && (
           <div className="text-lg">
             <div> 1st Place: { groups[groups.length - 1].name}</div>
-            <div> 2nd Place: { groups[groups.length - 2].name}</div>
-            <div> 3rd Place: { groups[groups.length - 3].name}</div>
           </div>
         )}
       </div>
@@ -106,7 +69,7 @@ const LeaderboardPage: React.FC = () => {
       {/* Display cars */}
       <div className="flex-grow flex items-center justify-center px-4">
         <div className="overflow-x-auto">
-          <div className="flex w-max items-end space-x-6 py-12 px-6 bg-gray-100 rounded-lg shadow-inner h-50">
+          <div className="flex w-max items-end space-x-6 py-12 px-6 bg-gray-100 rounded-lg shadow-inner h-45">
             {groups.map((group, index) => (
               <Car key={index} name={group.name} points={group.points} carImage={group.carImage} />
             ))}
